a20b579b9730f07a0764a5d924bad29d0b0d7b75
Sofie Benjaminsen <sofiejb@gmail.com>
2025-11-02 16:04:29 +0100

    nye terraform ressurser

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,44 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.40.0"
-    }
-  }
-  backend "azurerm" {
-  }
+# Random suffix for unique naming
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "fd-rg" {
-  name     = var.rg_name
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Project     = var.project_name
+    Test        = "TestValue"
+    Test2       = "TestValue2"
+  }
+}
+
+# Storage Account
+resource "azurerm_storage_account" "main" {
+  name                = "st${var.project_name}${var.environment}${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version = "TLS1_2"
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Storage Container
+resource "azurerm_storage_container" "demo" {
+  name                  = "demo-data"
+  storage_account_id    = azurerm_storage_account.main.id
+  container_access_type = "private"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,21 @@
-variable "rg_name" {
-  description = "Name of resource group"
+variable "environment" {
+  description = "Environment name (dev, test, prod)"
   type        = string
+
+  validation {
+    condition     = contains(["dev", "test", "prod"], var.environment)
+    error_message = "Environment must be dev, test, or prod."
+  }
 }
 
 variable "location" {
-  description = "location for resource group"
+  description = "Azure region for resources"
   type        = string
+  default     = "norwayeast"
+}
+
+variable "project_name" {
+  description = "Project name used in resource naming"
+  type        = string
+  default     = "test-deploy"
 }
